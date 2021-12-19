0e9c01f5407c7c4c5afcc9dee4be372af747f227
adityakadagala <64749169+adityakadagala@users.noreply.github.com>
2021-12-19 14:00:51 +0530

    Update Order_book_system2.sql

--- Order_book_system2.sql
+++ Order_book_system2.sql
@@ -23,75 +23,28 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `Admin_name` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+
 -- Dumping data for table order_book_system.admin: ~0 rows (approximately)
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 
 -- Dumping structure for table order_book_system.customer
 CREATE TABLE IF NOT EXISTS `customer` (
-  `Stock_name` varchar(15) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
+  `Customer_name` varchar(15) DEFAULT NULL,
+  `Customer_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `customer` (`Customer_name`, `Customer_id`) VALUES
+	('Hrudeestha', 9831)
+	('Aditya', 9123);
+
+
 
 -- Dumping data for table order_book_system.customer: ~0 rows (approximately)
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-
--- Dumping structure for table order_book_system.customer_order
-CREATE TABLE IF NOT EXISTS `customer_order` (
-  `order_id` int(11) NOT NULL,
-  `Stock_name` varchar(10) DEFAULT NULL,
-  `order_quantity` int(11) DEFAULT NULL,
-  `order_type` varchar(10) DEFAULT NULL,
-  `Executed_qnty` int(11) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
-  `order_status` varchar(10) DEFAULT NULL,
-  `order_date` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table order_book_system.customer_order: ~9 rows (approximately)
-/*!40000 ALTER TABLE `customer_order` DISABLE KEYS */;
-INSERT INTO `customer_order` (`order_id`, `Stock_name`, `order_quantity`, `order_type`, `Executed_qnty`, `price`, `order_status`, `order_date`) VALUES
-	(1, 'DBS', 50, 'LIMIT', 0, 150, 'PLACED', '21FEB2021'),
-	(2, 'DBS', 50, 'market', 0, NULL, 'PLACED', '21FEB2021'),
-	(3, 'DBS', 50, 'LIMIT', 0, 140, 'PLACED', '21FEB2021'),
-	(7, 'DBS', 50, 'limit', 50, 150, 'EXECUTED', '21FEB2021'),
-	(8, 'DBS', 50, 'MARKET', 50, NULL, 'EXECUTED', '21FEB2021');
-/*!40000 ALTER TABLE `customer_order` ENABLE KEYS */;
-
--- Dumping structure for table order_book_system.order_customer
-CREATE TABLE IF NOT EXISTS `order_customer` (
-  `cust_id` int(11) DEFAULT NULL,
-  `Stock_id` int(11) DEFAULT NULL,
-  `Order_type` varchar(50) DEFAULT NULL,
-  `Quantity` int(11) DEFAULT NULL,
-  `Date` date DEFAULT NULL,
-  `Price` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table order_book_system.order_customer: ~0 rows (approximately)
-/*!40000 ALTER TABLE `order_customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_customer` ENABLE KEYS */;
-
--- Dumping structure for table order_book_system.order_execution
-CREATE TABLE IF NOT EXISTS `order_execution` (
-  `Sno` int(11) DEFAULT NULL,
-  `Stock_name` varchar(50) DEFAULT NULL,
-  `Order_qty` int(11) DEFAULT NULL,
-  `Order_type` varchar(50) DEFAULT NULL,
-  `Exec_qty` int(11) DEFAULT NULL,
-  `Price` int(11) DEFAULT NULL,
-  `Order_status` varchar(50) DEFAULT NULL,
-  `Order_date` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table order_book_system.order_execution: ~0 rows (approximately)
-/*!40000 ALTER TABLE `order_execution` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_execution` ENABLE KEYS */;
-
--- Dumping structure for table order_book_system.stock
 CREATE TABLE IF NOT EXISTS `stock` (
   `Stock_name` varchar(50) DEFAULT NULL,
   `Stock_id` int(11) DEFAULT NULL
@@ -105,6 +58,75 @@ INSERT INTO `stock` (`Stock_name`, `Stock_id`) VALUES
 	('jewellery', 121),
 	('KCS', 164),
 	('BRS', 102);
+
+-- Dumping structure for table order_book_system.customer_order
+CREATE TABLE IF NOT EXISTS `customer_order` (
+  `order_id` int(11) NOT NULL,
+  `Stock_name` varchar(10) DEFAULT NULL,
+  `Order_qty` int(11) DEFAULT NULL,
+  `Order_type` varchar(10) DEFAULT NULL,
+  `Exec_qty` int(11) DEFAULT NULL,
+  `Price` int(11) DEFAULT NULL,
+  `Order_status` varchar(10) DEFAULT NULL,
+  `Order_date` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table order_book_system.customer_order: ~8 rows (approximately)
+/*!40000 ALTER TABLE `customer_order` DISABLE KEYS */;
+INSERT INTO `customer_order` (`Order_id`, `Stock_name`, `Order_qty`, `Order_type`, `Exec_qty`, `Price`, `Order_status`, `Order_date`) VALUES
+	(1, 'DBS', 50, 'LIMIT', 0, 150, 'PLACED', '21FEB2021'),
+	(2, 'DBS', 50, 'market', 0, NULL, 'PLACED', '21FEB2021'),
+	(3, 'DBS', 50, 'LIMIT', 0, 140, 'PLACED', '21FEB2021'),
+	
+/*!40000 ALTER TABLE `customer_order` ENABLE KEYS */;
+
+-- Dumping structure for table order_book_system.order_customer
+
+-- Dumping data for table order_book_system.order_customer: ~0 rows (approximately)
+/*!40000 ALTER TABLE `order_customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_customer` ENABLE KEYS */;
+
+-- Dumping structure for table order_book_system.order_execution
+CREATE TABLE IF NOT EXISTS `order_execution1` (
+  `Order_id` int(11) DEFAULT NULL,
+  `Stock_name` varchar(50) DEFAULT NULL,
+  `Order_qty` int(11) DEFAULT NULL,
+  `Order_type` varchar(50) DEFAULT NULL,
+  `Exec_qty` int(11) DEFAULT NULL,
+  `Price` int(11) DEFAULT NULL,
+  `Order_status` varchar(50) DEFAULT NULL,
+  `Order_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `order_execution1` (`Order_id`, `Stock_name`, `Order_qty`, `Order_type`, `Exec_qty`, `Price`, `Order_status`, `Order_date`) VALUES
+	(1, 'DBS', 50, 'LIMIT', 30, 150, 'ACCEPTED', '21FEB2021'),
+	(2, 'DBS', 50, 'market', 30, NULL, 'ACCEPTED', '21FEB2021'),
+	(3, 'DBS', 50, 'limit', 0, 140, 'REJECTED', '21FEB2021');
+	
+CREATE TABLE IF NOT EXISTS `order_execution2` (
+  `Sno` int(11) DEFAULT NULL,
+  `Stock_name` varchar(50) DEFAULT NULL,
+  `Order_qty` int(11) DEFAULT NULL,
+  `Order_type` varchar(50) DEFAULT NULL,
+  `Exec_qty` int(11) DEFAULT NULL,
+  `Price` int(11) DEFAULT NULL,
+  `Order_status` varchar(50) DEFAULT NULL,
+  `Order_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;	
+
+INSERT INTO `order_execution2` (`Sno`,`Order_id`, `Stock_name`, `Order_qty`, `Order_type`, `Exec_qty`, `Price`, `Order_status`, `Order_date`) VALUES
+	(1, 'DBS', 50, 'limit', 50, 150, 'EXECUTED', '21FEB2021'),
+	(2, 'DBS', 50, 'MARKET', 50, NULL, 'EXECUTED', '21FEB2021'),
+	(3, 'DBS', 50, 'LIMIT', 0, 140, 'REJECTED', '21FEB2021');
+
+
+-- Dumping data for table order_book_system.order_execution: ~0 rows (approximately)
+/*!40000 ALTER TABLE `order_execution` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_execution` ENABLE KEYS */;
+
+-- Dumping structure for table order_book_system.stock
+
 /*!40000 ALTER TABLE `stock` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
